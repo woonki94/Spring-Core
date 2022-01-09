2e10c9f048fd5a90a24566b842252dd2fcfe9396
woonki94 <woonki94@naver.com>
2022-01-09 21:06:54 +0900

    스프링으로 전환

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,24 +9,30 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
     /***생성자 주입
      * 어떤 구현 객체가 들어올지는 APPConfig에 의해서만 결정된다.
      * */
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
 
     }
-
+    @Bean
     public DiscountPolicy discountPolicy(){
        // return new FixDiscountPolicy();
         return new RateDiscountPolicy();
