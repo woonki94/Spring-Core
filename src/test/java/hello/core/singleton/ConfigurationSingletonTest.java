df0d7ece10773931c4dcd648eb06a453d252a7a8
woonki94 <woonki94@naver.com>
2022-01-12 13:10:32 +0900

    singleton pattern

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -36,8 +36,12 @@ public class ConfigurationSingletonTest {
     void configurationDeep(){
         ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
         AppConfig bean = ac.getBean(AppConfig.class);
-
         System.out.println("bean = " + bean.getClass());
     }
+
+
+
+
+
 
 }
